c2e28bd7b451406e3ae9900f719456c0c09e665f
run27017 <run27017@126.com>
2018-09-14 17:52:18 +0800

    多功能的JSONToTable

--- src/jsonToTable/index.js
+++ src/jsonToTable/index.js
@@ -1,3 +1,15 @@
 const jsonToTable = require('./jsonToTable')
+const Meta = require('../Meta')
+const dataToMeta = require('../dataToMeta')
+
+function intelligentJSONToTable (meta = null, data, builder) {
+  if (meta === null) {
+    meta = dataToMeta(meta)
+  } else if (!(meta instanceof Meta)) {
+    meta = new Meta(meta)
+  }
+
+  return jsonToTable(meta, data, builder)
+}
 
 module.exports = jsonToTable
