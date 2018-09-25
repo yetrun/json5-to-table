169f86f8d40a2fc6c84c66edffdde97e6d56eeb0
run27017 <run27017@126.com>
2018-09-25 18:40:07 +0800

    导出dataToMeta属性

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+const dataToMeta = require('./dataToMeta')
 const jsonToTable = require('./jsonToTable')
 const jsonToHTML = require('./jsonToHTML')
 
 module.exports = {
   jsonToTable,
-  jsonToHTML
+  jsonToHTML,
+  dataToMeta
 }
