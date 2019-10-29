f01e36df8f4e20f8f0d575d4117c1c8875218d00
run27017 <run27017@126.com>
2019-10-29 15:56:59 +0800

    为 nested 的数据生成行

--- test/generateRow.test.js
+++ test/generateRow.test.js
@@ -13,3 +13,23 @@ test('simple generate', t => {
     { r: 1, c: 3, v: 3 }
   ])
 })
+
+test('nested generate', t => {
+  const props = [
+    { key: 'a' },
+    { 
+      key: 'b', 
+      props: [ { key: 'c' }, { key: 'd' } ]
+    },
+    { key: 'e' }
+  ]
+  const data = { a: 1, b: { c: 2, d: 3 }, e: 4 }
+
+  const cells = generateRow(data, props)
+  t.deepEqual(cells, [
+    { r: 1, c: 1, v: 1 },
+    { r: 1, c: 2, v: 2 },
+    { r: 1, c: 3, v: 3 },
+    { r: 1, c: 4, v: 4 }
+  ])
+})
