e2eef980c57e63b3f68af527880b813577ac820b
run27017 <run27017@126.com>
2019-10-29 11:26:04 +0800

    完成最简单的 Render 的测试逻辑

---
+++ test/render1.test.js
@@ -0,0 +1,28 @@
+import test from 'ava'
+import Render1 from '../lib/Render1'
+
+test('simple render', t => {
+  const render = new Render1()
+  const data = [
+    { a: 1, b: 2, c: 3 },
+    { a: 4, b: 5, c: 6 }
+  ]
+
+  render.init(data)
+  t.deepEqual(render.next(), { value: [
+    { r: 1, c: 1, v: 'a' },
+    { r: 1, c: 2, v: 'b' },
+    { r: 1, c: 3, v: 'c' }
+  ], done: false })
+  t.deepEqual(render.next(), { value: [
+    { r: 2, c: 1, v: 1 },
+    { r: 2, c: 2, v: 2 },
+    { r: 2, c: 3, v: 3 }
+  ], done: false })
+  t.deepEqual(render.next(), { value: [
+    { r: 3, c: 1, v: 4 },
+    { r: 3, c: 2, v: 5 },
+    { r: 3, c: 3, v: 6 }
+  ], done: true })
+  t.deepEqual(render.next(), { value: undefined, done: true })
+})
