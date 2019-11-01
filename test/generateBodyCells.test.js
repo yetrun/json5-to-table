76cf540fa82280635945ebefdb794c4f02e3dcda
run27017 <run27017@126.com>
2019-11-01 15:05:17 +0800

    generateBodyCells: 同时存在两个嵌套数组

--- test/generateBodyCells.test.js
+++ test/generateBodyCells.test.js
@@ -81,3 +81,47 @@ test('nested array generate', t => {
     { r: 2, c: 3, v: 5 }
   ))
 })
+
+test('two nested array generate', t => {
+  const props = [
+    { key: 'a' },
+    { 
+      key: 'b', 
+      props: [ { key: 'd' }, { key: 'e' } ]
+    },
+    { 
+      key: 'c', 
+      props: [ { key: 'd' }, { key: 'e' }, { key: 'f' } ]
+    }
+  ]
+  const data = {
+    a: 1,
+    b: [
+      { d: 2, e: 3 },
+      { d: 4, e: 5 }
+    ],
+    c: [
+      { d: 6, e: 7, f: 8 },
+      { d: 9, e: 10, f: 11 },
+      { d: 12, e: 13, f: 14 }
+    ]
+  }
+
+  const cells = generateCells(data, props)
+  t.deepEqual(cells, AddressableCells(    
+    { r: 1, c: 1, rs: 3, v: 1 },
+    { r: 1, c: 2, v: 2 },
+    { r: 1, c: 3, v: 3 },
+    { r: 2, c: 2, rs: 2, v: 4 },
+    { r: 2, c: 3, rs: 2, v: 5 },
+    { r: 1, c: 4, v: 6 },
+    { r: 1, c: 5, v: 7 },
+    { r: 1, c: 6, v: 8 },
+    { r: 2, c: 4, v: 9 },
+    { r: 2, c: 5, v: 10 },
+    { r: 2, c: 6, v: 11 },
+    { r: 3, c: 4, v: 12 },
+    { r: 3, c: 5, v: 13 },
+    { r: 3, c: 6, v: 14 }
+  ))
+})
--- test/generateTable.test.js
+++ test/generateTable.test.js
@@ -79,3 +79,45 @@ test('nested array generate', t => {
     ]
   ))
 })
+
+test('two nested array generate', t => {
+  const props = [
+    { key: 'a' },
+    { 
+      key: 'b', 
+      props: [ { key: 'd' }, { key: 'e' } ]
+    },
+    { 
+      key: 'c', 
+      props: [ { key: 'd' }, { key: 'e' }, { key: 'f' } ]
+    }
+  ]
+  const data = {
+    a: 1,
+    b: [
+      { d: 2, e: 3 },
+      { d: 4, e: 5 }
+    ],
+    c: [
+      { d: 6, e: 7, f: 8 },
+      { d: 9, e: 10, f: 11 },
+      { d: 12, e: 13, f: 14 }
+    ]
+  }
+
+  const table = generateTable(data, props)
+  t.deepEqual(table, new Table(
+    [
+      1,
+      [
+        [2, 3],
+        [4, 5]
+      ],
+      [
+        [6, 7, 8],
+        [9, 10, 11],
+        [12, 13, 14]
+      ]
+    ]
+  ))
+})
