0e14d7427d2b94e558c38565dadf69efaa29146a
run27017 <run27017@126.com>
2019-11-05 15:50:06 +0800

    给到 jsonToExcel 的比较测试

--- test/jsonToExcel.test.js
+++ test/jsonToExcel.test.js
@@ -1,5 +1,18 @@
 const test = require('./test')
+const XLSX = require('xlsx')
+const fs = require('fs')
+const path = require('path')
 const jsonToExcel = require('../lib/jsonToExcel')
+
+function generateSameXLSX (t, sourcePath, ws) {
+  const wb = XLSX.utils.book_new()
+  XLSX.utils.book_append_sheet(wb, ws, 'Sheet1')
+
+  const filename = path.basename(sourcePath).replace(path.extname(sourcePath), '')
+  const targetPath = path.dirname(sourcePath) + '/' + filename + '.dump' + path.extname(sourcePath)
+  XLSX.writeFile(wb, targetPath)
+  t.deepEqual(fs.readFileSync(sourcePath), fs.readFileSync(targetPath))
+}
 
 test('simple generate', t => {
   const props = [ { key: 'a' }, { key: 'b' }, { key: 'c' } ]
@@ -9,7 +22,19 @@ test('simple generate', t => {
   ]
 
   jsonToExcel(data, props, 'test/temp/1.xlsx')
-  t.pass()
+  generateSameXLSX(t, 'test/temp/1.xlsx', {
+    A1: { v: 'a', t: 's' },
+    B1: { v: 'b', t: 's' },
+    C1: { v: 'c', t: 's' },
+    A2: { v: 1, t: 'n' },
+    B2: { v: 2, t: 'n' },
+    C2: { v: 3, t: 'n' },
+    A3: { v: 4, t: 'n' },
+    B3: { v: 5, t: 'n' },
+    C3: { v: 6, t: 'n' },
+    '!merges': [],
+    '!ref': 'A1:C3'
+  })
 })
 
 test('complex generate', t => {
@@ -41,7 +66,35 @@ test('complex generate', t => {
   ]
 
   jsonToExcel(data, props, 'test/temp/2.xlsx')
-  t.pass()
+  generateSameXLSX(t, 'test/temp/2.xlsx', {
+    A1: { v: 'a', t: 's' },
+    B1: { v: 'b', t: 's' },
+    D1: { v: 'e', t: 's' },
+    B2: { v: 'c', t: 's' },
+    C2: { v: 'd', t: 's' },
+    A3: { v: 1, t: 'n' },
+    B3: { v: 2, t: 'n' },
+    C3: { v: 3, t: 'n' },
+    D3: { v: 6, t: 'n' },
+    B4: { v: 4, t: 'n' },
+    C4: { v: 5, t: 'n' },
+    A5: { v: 11, t: 'n' },
+    B5: { v: 12, t: 'n' },
+    C5: { v: 13, t: 'n' },
+    D5: { v: 16, t: 'n' },
+    B6: { v: 14, t: 'n' },
+    C6: { v: 15, t: 'n' },
+    '!merges':[ 
+      { s: { r: 0, c: 0 }, e: { r: 1, c: 0 } },
+      { s: { r: 0, c: 1 }, e: { r: 0, c: 2 } },
+      { s: { r: 0, c: 3 }, e: { r: 1, c: 3 } },
+      { s: { r: 2, c: 0 }, e: { r: 3, c: 0 } },
+      { s: { r: 2, c: 3 }, e: { r: 3, c: 3 } },
+      { s: { r: 4, c: 0 }, e: { r: 5, c: 0 } },
+      { s: { r: 4, c: 3 }, e: { r: 5, c: 3 } } 
+    ],
+    '!ref': 'A1:D6',
+  })
 })
 
 test('with blank space', t => {
@@ -72,10 +125,41 @@ test('with blank space', t => {
   ]
 
   jsonToExcel(data, props, 'test/temp/3.xlsx')
-  t.pass()
+  generateSameXLSX(t, 'test/temp/3.xlsx', { 
+    A1: { v: 'a', t: 's' },
+    B1: { v: 'b', t: 's' },
+    D1: { v: 'c', t: 's' },
+    B2: { v: 'd', t: 's' },
+    C2: { v: 'e', t: 's' },
+    D2: { v: 'd', t: 's' },
+    E2: { v: 'e', t: 's' },
+    F2: { v: 'f', t: 's' },
+    A3: { v: 1, t: 'n' },
+    B3: { v: 2, t: 'n' },
+    C3: { v: 3, t: 'n' },
+    D3: { v: 6, t: 'n' },
+    E3: { v: 7, t: 'n' },
+    F3: { v: 8, t: 'n' },
+    B4: { v: 4, t: 'n' },
+    C4: { v: 5, t: 'n' },
+    D4: { v: 9, t: 'n' },
+    E4: { v: 10, t: 'n' },
+    F4: { v: 11, t: 'n' },
+    D5: { v: 12, t: 'n' },
+    E5: { v: 13, t: 'n' },
+    F5: { v: 14, t: 'n' },
+    '!merges':[ 
+      { s: { r: 0, c: 0 }, e: { r: 1, c: 0 } },
+      { s: { r: 0, c: 1 }, e: { r: 0, c: 2 } },
+      { s: { r: 0, c: 3 }, e: { r: 0, c: 5 } },
+      { s: { r: 2, c: 0 }, e: { r: 4, c: 0 } },
+      { s: { r: 4, c: 1 }, e: { r: 4, c: 2 } } 
+    ],
+    '!ref': 'A1:F5',
+  })
 })
 
-test('with array and object val', t => {
+test('with array and object value', t => {
   const props = [ { key: 'a' }, { key: 'b' }, { key: 'c' } ]
   const data = [
     { a: 1, b: [2.1, 2.2, 2.3], c: 3 },
@@ -83,5 +167,17 @@ test('with array and object val', t => {
   ]
 
   jsonToExcel(data, props, 'test/temp/4.xlsx')
-  t.pass()
+  generateSameXLSX(t, 'test/temp/4.xlsx',{
+    A1: { v: 'a', t: 's' },
+    B1: { v: 'b', t: 's' },
+    C1: { v: 'c', t: 's' },
+    A2: { v: 1, t: 'n' },
+    B2: { v: '2.1, 2.2, 2.3', t: 's' },
+    C2: { v: 3, t: 'n' },
+    A3: { v: 4, t: 'n' },
+    B3: { v: '{"k1":5.1,"k2":5.2}', t: 's' },
+    C3: { v: 6, t: 'n' },
+    '!merges': [],
+    '!ref': 'A1:C3',
+  })
 })
