ef49d87a2e11a1abc82ffd3192bac7aa1c1e644f
run27017 <run27017@126.com>
2019-11-06 14:36:29 +0800

    jsonToHTMLTable 面向浏览器实现

---
+++ demo/json-to-html-table.js
@@ -0,0 +1,91 @@
+const jsonToHTMLTable = require('../lib/jsonToHTMLTable')
+
+function table1 () {
+  const props = [ { key: 'a' }, { key: 'b' }, { key: 'c' } ]
+  const data = [
+    { a: 1, b: 2, c: 3 },
+    { a: 4, b: 5, c: 6 }
+  ]
+
+  const table = jsonToHTMLTable(data, props)
+  document.getElementById('table1').appendChild(table)
+}
+
+function table2 () {
+  const props = [
+    { key: 'a' },
+    { 
+      key: 'b', 
+      props: [ { key: 'c' }, { key: 'd' } ]
+    },
+    { key: 'e' }
+  ]
+  const data = [
+    {
+      a: 1,
+      b: [
+        { c: 2, d: 3 },
+        { c: 4, d: 5 }
+      ],
+      e: 6
+    },
+    {
+      a: 11,
+      b: [
+        { c: 12, d: 13 },
+        { c: 14, d: 15 }
+      ],
+      e: 16
+    }
+  ]
+
+  const table = jsonToHTMLTable(data, props)
+  document.getElementById('table2').appendChild(table)
+}
+
+function table3 () {
+  const props = [
+    { key: 'a' },
+    { 
+      key: 'b', 
+      props: [ { key: 'd' }, { key: 'e' } ]
+    },
+    { 
+      key: 'c', 
+      props: [ { key: 'd' }, { key: 'e' }, { key: 'f' } ]
+    }
+  ]
+  const data = [
+    {
+      a: 1,
+      b: [
+        { d: 2, e: 3 },
+        { d: 4, e: 5 }
+      ],
+      c: [
+        { d: 6, e: 7, f: 8 },
+        { d: 9, e: 10, f: 11 },
+        { d: 12, e: 13, f: 14 }
+      ]
+    }
+  ]
+
+  const table = jsonToHTMLTable(data, props)
+  document.getElementById('table3').appendChild(table)
+}
+
+function table4 () {
+  const props = [ { key: 'a' }, { key: 'b' }, { key: 'c' } ]
+  const data = [
+    { a: 1, b: [2.1, 2.2, 2.3], c: 3 },
+    { a: 4, b: { k1: 5.1, k2: 5.2 }, c: 6 }
+  ]
+
+  const table = jsonToHTMLTable(data, props)
+  document.getElementById('table4').appendChild(table)
+}
+
+table1()
+table2()
+table3()
+table4()
---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mode: 'development',
+  entry: './demo/json-to-html-table.js',
+  output: {
+    filename: 'json-to-html-table-demo.js'
+  }
+}
