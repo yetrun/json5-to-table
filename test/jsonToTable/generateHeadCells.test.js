1e1b1a8c9067837b5f27a3bd3099c1e266a8ff57
run27017 <run27017@126.com>
2020-02-07 16:33:03 +0800

    generateHeadCells 重构：平分多余空间

--- test/jsonToTable/generateHeadCells.test.js
+++ test/jsonToTable/generateHeadCells.test.js
@@ -62,14 +62,55 @@ test('props -> props,(props -> props)', t => {
   const cells = generateCells(props)
   t.deepEqual(cells, new AddressableCells(
     { r: 1, c: 1, rs: 3, v: 'a' },
-    { r: 1, c: 2, cs: 2, v: 'b' },
-    { r: 2, c: 2, rs: 2, v: 'c' },
-    { r: 2, c: 3, rs: 2, v: 'd' },
+    { r: 1, c: 2, rs: 2, cs: 2, v: 'b' },
+    { r: 3, c: 2, rs: 1, v: 'c' },
+    { r: 3, c: 3, rs: 1, v: 'd' },
     { r: 1, c: 4, cs: 3, v: 'e' },
     { r: 2, c: 4, cs: 2, v: 'f' },
     { r: 3, c: 4, v: 'g' },
     { r: 3, c: 5, v: 'h' },
     { r: 2, c: 6, rs: 2, v: 'i' }
+  ))
+})
+
+test('allocation algorithm', t => {
+  const props = [
+    { key: 'a' },
+    { 
+      key: 'b', 
+      props: ['b1', 'b2']
+    },
+    { 
+      key: 'c',
+      props: [ 
+        { 
+          key: 'd',
+          props: [ 
+            { 
+              key: 'd1' ,
+              props: ['d1_1', 'd1_2']
+            }, 
+            { key: 'd2' }
+          ]
+        }, 
+        { key: 'e' } 
+      ]
+    }
+  ]
+
+  const cells = generateCells(props)
+  t.deepEqual(cells, new AddressableCells(
+    { r: 1, c: 1, rs: 4, v: 'a' },
+    { r: 1, c: 2, rs: 2, cs: 2, v: 'b' },
+    { r: 3, c: 2, rs: 2, v: 'b1' },
+    { r: 3, c: 3, rs: 2, v: 'b2' },
+    { r: 1, c: 4, cs: 4, v: 'c' },
+    { r: 2, c: 4, cs: 3, v: 'd' },
+    { r: 2, c: 7, rs: 3, cs: 1, v: 'e' },
+    { r: 3, c: 4, cs: 2, v: 'd1' },
+    { r: 3, c: 6, rs: 2, v: 'd2' },
+    { r: 4, c: 4, v: 'd1_1' },
+    { r: 4, c: 5, v: 'd1_2' }
   ))
 })
 
